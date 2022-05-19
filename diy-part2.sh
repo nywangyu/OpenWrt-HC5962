7a60556c4e94cda157b9487e36e15b3acf4e8449
nywangyu <dfacny@sina.com>
2022-05-19 16:19:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,3 +15,4 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 
 rm -rf lede/package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+svn co https://github.com/kenzok8/small-package/trunk/luci-app-tencentddns package/luci-app-tencentddns
