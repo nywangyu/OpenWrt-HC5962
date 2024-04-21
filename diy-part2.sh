7d5613e06ad5b616912c4d0b1d6165a57ea69090
nywangyu <dfacny@sina.com>
2024-04-21 21:36:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,9 +13,9 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
-git clone -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
-git clone https://github.com/1wrt/luci-app-ikoolproxy.git package/luci-app-ikoolproxy
-rm -rf lede/package/lean/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git clone https://github.com/TorBoxCode/luci-app-tencentddns package/luci-app-tencentddns
-git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+# git clone -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
+# git clone https://github.com/1wrt/luci-app-ikoolproxy.git package/luci-app-ikoolproxy
+# rm -rf lede/package/lean/luci-theme-argon
+# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+# git clone https://github.com/TorBoxCode/luci-app-tencentddns package/luci-app-tencentddns
+# git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
