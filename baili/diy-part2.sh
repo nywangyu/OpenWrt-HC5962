7a14c1e8ef9a00d0e9f40b51c49a5decbb6f9755
nywangyu <dfacny@sina.com>
2025-02-17 15:17:13 +0800

    Update diy-part2.sh

--- baili/diy-part2.sh
+++ baili/diy-part2.sh
@@ -16,8 +16,8 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 git clone https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
 #git clone https://github.com/1wrt/luci-app-ikoolproxy.git package/luci-app-ikoolproxy
 rm -rf package/lean/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 rm -rf package/lean/luci-app-adbyby-plus
 git clone https://github.com/ywt114/luci-app-adbyby-plus-lite package/luci-app-adbyby-plus
 ##-----------------Manually set CPU frequency for MT7986A-----------------
-sed -i '/"mediatek"\/\*|\"mvebu"\/\*/{n; s/.*/\tcpu_freq="2.0GHz" ;;/}' package/emortal/autocore/files/generic/cpuinfo
+#sed -i '/"mediatek"\/\*|\"mvebu"\/\*/{n; s/.*/\tcpu_freq="2.0GHz" ;;/}' package/emortal/autocore/files/generic/cpuinfo
