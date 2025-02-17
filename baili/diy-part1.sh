97bfbbf10a5a774282ad21565afbb3d263c22712
nywangyu <dfacny@sina.com>
2025-02-17 15:27:50 +0800

    Update diy-part1.sh

--- baili/diy-part1.sh
+++ baili/diy-part1.sh
@@ -16,5 +16,6 @@
 # Add a feed source
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
