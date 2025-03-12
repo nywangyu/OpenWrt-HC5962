50a848c392e4b64c31ba1de6379d45f8d3e4ab6b
nywangyu <dfacny@sina.com>
2025-03-12 22:05:03 +0800

    Update diy-part2.sh

--- 360v6/diy-part2.sh
+++ 360v6/diy-part2.sh
@@ -11,11 +11,11 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
-git clone https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
-git clone https://github.com/1wrt/luci-app-ikoolproxy.git package/luci-app-ikoolproxy
-rm -rf package/lean/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git clone https://github.com/TorBoxCode/luci-app-tencentddns package/luci-app-tencentddns
-git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+#git clone https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
+#git clone https://github.com/1wrt/luci-app-ikoolproxy.git package/luci-app-ikoolproxy
+#rm -rf package/lean/luci-theme-argon
+#git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+#git clone https://github.com/TorBoxCode/luci-app-tencentddns package/luci-app-tencentddns
+#git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
