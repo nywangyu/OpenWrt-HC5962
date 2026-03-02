f16695a7b2c2294374f12012463af898367e1a42
nywangyu <lbxshu@163.com>
2026-03-02 15:55:40 +0800

    Add files via upload

---
+++ build/immortalwrt/baili/diy-part2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+git clone -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
+#git clone https://github.com/1wrt/luci-app-ikoolproxy.git package/luci-app-ikoolproxy
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+rm -rf package/lean/luci-app-adbyby-plus
+git clone https://github.com/ywt114/luci-app-adbyby-plus-lite package/luci-app-adbyby-plus
+##-----------------Manually set CPU frequency for MT7986A-----------------
+#sed -i '/"mediatek"\/\*|\"mvebu"\/\*/{n; s/.*/\tcpu_freq="2.0GHz" ;;/}' package/emortal/autocore/files/generic/cpuinfo
